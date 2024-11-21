34bda0df4688a3542f58d770e2037f8e06a62c8e
Thomas Deroo <neromule@gmail.com>
2024-11-21 01:06:03 +0100

    Source arg

--- src/main.py
+++ src/main.py
@@ -30,6 +30,9 @@ async def get_artifact(id: str):
 
     if 'token' in request.args:
         if request.args['token'] == a.token:
+            if 'source' in request.args:
+                return a.content
+
             if request.accept_mimetypes is not None:
                 best_match = request.accept_mimetypes.best_match(['text/html', 'application/json'])
 
